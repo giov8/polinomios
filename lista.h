f8e1243a80da396b288d327db8c7ee1f92a6dc37
marisasel <marisafranco@gmail.com>
2018-10-24 22:17:53 -0200

    lista.h

--- lista.h
+++ lista.h
@@ -3,4 +3,16 @@
 
 /* PREENCHER COM SUAS DEFINIÇÕES DE TIPOS E FUNÇÕES */
 
+	typedef struct termo *TipoPonteiro;     // define um TipoPonteiro para nodes de termos do polinomio
+
+	typedef struct termo {                  // define o tipo termo
+		int coeficiente;                // esse campo vai indicar o coeficiente
+		TipoPonteiro prox;              // esse campo vai apontar para o proximo node
+	} termo;
+
+        typedef struct lista {
+                int tam;                        // esse campo vai indicar o numero de nos (graus) da lista (polinomio)
+		TipoPonteiro prim;              // esse campo vai apontar para o primeiro node
+		TipoPonteiro ult;               // esse campo vai apontar para o ultimo node
+        } lista;
 #endif
